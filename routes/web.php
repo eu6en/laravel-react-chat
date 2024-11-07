fe85d82a69906922599f41647f27e5e66ad5ac5a
eu6en <46784927+eu6en@users.noreply.github.com>
2024-11-07 15:38:38 -0500

    Group routes in web.php under auth middleware to prevent unauthorized access to the pages

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,28 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 
-Route::get('/', function () {
-    return Inertia::render('Chats');
-})->middleware(['auth', 'verified'])->name('chats');
-Route::get('/chats', function () {
-    return redirect('/');
-});
+    Route::get('/', function () {
+        return Inertia::render('Chats');
+    })->name('chats');
 
-Route::get('/chats/{chatId}', function ($chatId) {
-    return Inertia::render('Chat', ['chatId' => $chatId]);
-});
+    Route::get('/chats', function () {
+        return redirect('/');
+    });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/chats/{chatId}', function ($chatId) {
+        return Inertia::render('Chat', ['chatId' => $chatId]);
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 });
 
 Route::get('/test-react-page', function () {
